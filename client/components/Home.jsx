bba84c3b6dc9216c9d313501c3c50de5dba6edeb
ben-marrett <benteiko@gmail.com>
2022-04-07 13:57:54 +1200

    clear out template and hello world home route

---
+++ client/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      <p>Kia ora, welcome to the pātaka kai app.</p>
+    </div>
+  )
+}
+
+export default Home
