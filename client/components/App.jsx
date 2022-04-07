c65ae81c18e31370daf5316129f56eda5a289093
ben-marrett <benteiko@gmail.com>
2022-04-07 14:00:27 +1200

    remove old code from template

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,26 +1,10 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 
 import Home from './Home'
-// import { getGreeting } from '../apiClient'
+
 
 const App = () => {
-  // const [greeting, setGreeting] = useState('')
-  // const [count, setCount] = useState(0)
-  // const [isError, setIsError] = useState(false)
 
-  // useEffect(() => {
-  //   getGreeting()
-  //     .then((greeting) => {
-  //       console.log(greeting)
-  //       setGreeting(greeting)
-  //       setIsError(false)
-  //       return null
-  //     })
-  //     .catch((err) => {
-  //       console.log(err)
-  //       setIsError(true)
-  //     })
-  // }, [count])
 
   return (
     <>
